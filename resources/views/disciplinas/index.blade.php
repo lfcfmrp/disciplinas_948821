92ace7af856d4e018b7994fb50a4ac5b85dd77a6
Luis Fernando <lfcozin@fmrp.usp.br>
2018-08-23 17:02:39 -0300

    Fixes #3 - Issue Resolvida - Método show()

--- resources/views/disciplinas/index.blade.php
+++ resources/views/disciplinas/index.blade.php
@@ -1,5 +1,7 @@
 <u1>
  @foreach($disciplinas as $disciplina)
-     <li>{{$disciplina->titulo}}</li>
+     <li>
+     <a href="/disciplinas/{{$disciplina->id}}">
+     {{$disciplina->titulo}}</li>
      @endforeach
 </u1>
