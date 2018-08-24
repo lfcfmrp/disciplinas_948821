090df33e50f1dc7d8dab8b56873cfa145c0279f5
Luis Fernando <lfcozin@fmrp.usp.br>
2018-08-24 14:14:48 -0300

    Fixes #4 - Issue Resolvida - Método create()

--- app/Http/Controllers/DisciplinaController.php
+++ app/Http/Controllers/DisciplinaController.php
@@ -26,7 +26,7 @@ class DisciplinaController extends Controller
      */
     public function create()
     {
-        //
+        return view('disciplinas.create');
     }
 
     /**
@@ -37,7 +37,12 @@ class DisciplinaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $disciplina = new Disciplina;
+        $disciplina->titulo = $request->titulo;
+        $disciplina->ementa = $request->ementa;
+        $disciplina->save();
+        return redirect('/');
+
     }
 
     /**
